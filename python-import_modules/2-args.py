2bc4b2419f88e30d6b847fc610975ed1691daf6b
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-03 10:28:54 +0000

    Task 1: Print arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)))
-        for i in range(1, len(argv)):
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1):
             print("{}: {}".format(i, argv[i]))
