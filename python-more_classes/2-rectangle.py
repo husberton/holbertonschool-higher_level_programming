4de35c8c7b3a0799dd66f17f53a52b8b2877500b
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-10 13:08:20 +0000

    saa

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -37,3 +37,9 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
