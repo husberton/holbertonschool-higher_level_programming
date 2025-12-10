c9c2ea1b12d250f22779cecb0e17eb6a8c0647b7
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-10 13:02:37 +0000

    Rectangle class with attributes and get/set

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-""" This is a class names Rectangle """
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """ It has the following attributes and methods """
+    """Represents a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize the rectangle."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrieve the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not isintance(value, int):
+        """Set width with validation."""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -22,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Retrieve the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
