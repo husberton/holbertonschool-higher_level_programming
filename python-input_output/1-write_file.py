9f4cf97400bbc77efaba90993895615b9b4bd856
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 12:47:36 +0000

    python

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" some comment """
+
+
+def write_file(filename="". text=""):
+    """ some code """
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write(text)
