7e16cfc221d9d52365ad8708ff13f1a749d5cf25
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 16:57:40 +0000

    py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ some code """
-import sys
+import sys, os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arguments = load_from_json_file('add_item.json')
+if os.path.exists('add_item.json'):
+    arguments = load_from_json_file('add_item.json')
 for arg in sys.argv[1:]:
     if arg not in arguments:
         arguments.append(arg)
