c0bad00777cc5beb93de53fbadd8fe5db58409e0
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 17:28:47 +0000

    py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            return {k:v for k in self.__dict__.items() if k in attrs}
+        return self.__dict__
