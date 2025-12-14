54ad70bae9c4fa98fa21bc89729c54c51103de02
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 09:40:00 +0000

    python

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,4 @@
 class MyList(list):
     """ This class customizes list """
     def print_sorted(self):
-        new_list = self[:]
-        new_list.sort()
-        print(new_list)
+        print(sorted(self))
