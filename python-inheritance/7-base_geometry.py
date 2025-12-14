18debcbadcd3b7fc32e6b2956dea1cfc9a5f216d
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 11:04:25 +0000

    python

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,9 +5,11 @@
 class BaseGeometry:
     """ geoclass """
     def area(self):
+        """ it is not implemented yet """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ validate integers here """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
