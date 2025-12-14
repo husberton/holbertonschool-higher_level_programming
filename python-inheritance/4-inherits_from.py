f01ef4a9406dfc8581d0ea0ef9f5b72216397b5f
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 10:13:49 +0000

    python

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ issubclass """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is a_class
