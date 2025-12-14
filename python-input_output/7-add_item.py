fb972014ec09beaecc34a184de930960da59dacd
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 16:47:15 +0000

    py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,5 +7,3 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arguments = sys.argv[1:]
 
 save_to_json_file(arguments, "add_item.json")
-with open("add_item.json", 'a', encoding='utf-8') as f:
-    f.write("\n")
