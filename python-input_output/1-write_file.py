60f304459ea33a6b33692a580cbd499aa51314df
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 12:49:22 +0000

    python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ some comment """
 
 
-def write_file(filename="". text=""):
+def write_file(filename="", text=""):
     """ some code """
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
