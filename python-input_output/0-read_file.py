43bac8b5f041655630d7d23ef3da109ae3c219fa
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 12:45:19 +0000

    python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 """ Some text """
 def read_file(filename=""):
     """ some code """
-    with open(filename, "r", "UTF8") as f:
-        f.read()
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
