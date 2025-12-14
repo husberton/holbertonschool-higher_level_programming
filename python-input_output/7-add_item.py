e76f4936b07512f9eb892fbc560ed4868b5f6715
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 16:55:07 +0000

    py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,6 +4,8 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arguments = sys.argv[1:]
-
+arguments = load_from_json_file('add_item.json')
+for arg in sys.argv[1:]:
+    if arg not in arguments:
+        arguments.append(arg)
 save_to_json_file(arguments, "add_item.json")
