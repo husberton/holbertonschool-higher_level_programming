6fc49ec38b9fa3b97ec45e46be44302562136c84
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 16:58:56 +0000

    py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ some code """
-import sys, os
+import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arguments = []
