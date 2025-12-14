c688bddee825c4b13aaa336e5ff6b3b365bb6047
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 17:21:57 +0000

    py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,8 +4,4 @@
 
 def class_to_json(obj):
     """ gjgj """
-    serialized = dict()
-    for attr in dir(obj):
-        if attr in [list, dict, str, int, bool]:
-            serialized[attr] = obj.attr
-    return str(serialized)
+    return obj.__dict__
