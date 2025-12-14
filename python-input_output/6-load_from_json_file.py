efbad321467e36f0d67c59bfc9ac23bab0373653
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-14 13:12:19 +0000

    python

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" sajdjasd """
+import json
+
+
+def load_from_json_file(filename):
+    """ asdfsadf """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
