27cd40920579188554013750c20b5f16ac2f0348
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-24 20:22:28 +0400

    py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import pickle
+import json
 """ Python serialization """
 
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+    return "The data has been serialized and saved to 'data.json'"
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
-    with open(filename, "rb") as file:
-        return pickle.load(file)
+    with open(filename, "r", encoding='utf-8') as file:
+        data = json.load(file)
+    return data
