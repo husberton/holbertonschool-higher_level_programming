c3238efd7f84f87bf10ed429df1beddf7bcafb41
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-24 21:08:21 +0400

    Task 1: Pickling classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import pickle
+""" Python serialization """
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    
+    def display(self):
+        return f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}"
+    
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as file:
+                return pickle.load(file) 
+        except FileNotFoundError:
+            return None
