99b16eeecf15d33166708b4565009c4d392b8f7b
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-25 21:57:06 +0400

    Fix SQL query to correctly order elements based on score from second_table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Display ordered elements based on score from the table 'second_table'
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC ;
