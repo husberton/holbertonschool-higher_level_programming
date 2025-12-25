49fbcb03a0606b6318657061b09f8c37cceaeca0
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-25 21:55:12 +0400

    Fix column order in SQL query to display score before name from second_table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Display ordered elements based on score from the table 'second_table'
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
